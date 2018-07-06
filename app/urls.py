2a98d2313d3aba75038b66febc644a3df74cc07a
JoaquinPerezA <joaquin.perez.a@ug.uchile.cl>
2018-07-06 05:35:50 -0400

    Creado sistema de usuarios, usando lo que tiene por defecto Django, agregadas páginas "beta" de login y register y realizados las migraciones de la DB (aunque debería cambar el modelo)

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
-app_name = 'app'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.test_page, name='index'),
+    path('login', views.login_page, name='loginPage'),
+    path('register', views.register_page, name='registerPage'),
+    path('registerUser', views.create_user, name='register'),
+    path('loginUser', views.login_user, name='login'),
+    re_path('.*', views.index),
 ]
