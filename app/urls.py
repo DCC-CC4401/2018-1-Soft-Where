97ed6323df3fe596d3b2fa7e658fb4494bcd94e2
Israel Peña <israel.pena@ing.uchile.cl>
2018-07-09 16:15:17 -0400

    Ficha Articulo

--- app/urls.py
+++ app/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login_page, name='loginPage'),
     path('register', views.register_page, name='registerPage'),
-    path('loginUser', views.login_user, name='login'),
+    path('login', views.login_page, name='login'),
     path('testpage', views.test_page, name='testPage'),
     path('adminlanding', views.admin_landing, name='adminLanding'),
     path('cambiar_estado_pendientes', views.cambiar_estado_pendientes, name='cambiar_estado_pendientes'),
     path('filtrar_prestamos', views.filtrar_prestamos, name='filtrar_prestamos'),
     re_path(r'^searchArticulos/$', views.search_articulos, name='searchArticulos'),
     # re_path(r'^cambiar_estado_pendientes/$', views.cambiar_estado_pendientes, name='cambiar_estado_pendientes'),
-    path('ficha_articulo', views.ficha_articulo, name='ficha_articulo')
+    path('ficha_articulo', views.ficha_articulo, name='ficha_articulo'),
+    path('profile', views.user_profile, name='userProfile'),
 ]
--- app/views.py
+++ app/views.py
@@ -11,6 +11,10 @@ import json
 # Muestra el indice
 def index(request):
     context = {'articulos': 'inicio'}
+    if request.user.is_authenticated:
+        context = {**context, **user_context(request)} # Esto fusiona dos dict
+    else:
+        pass
     return render(request, 'landing-page.html', context)
 
 
@@ -19,14 +23,38 @@ def test_page(request):
     return render(request, 'adminlanding.html')
 
 
-# Muestra la pagina de login si el usuario no esta logeado
+# Retorna los datos basicos del usuario logeado
+def user_context(request):
+    current_user=Usuario.objects.get(user=request.user)
+    context = {'name': str(current_user),
+               'rut' : current_user.rut,
+               'mail' : current_user.user.email}
+    return context
+
+
+def user_profile(request):
+    context = user_context(request)
+    return render(request, 'user_profile.html', context)
+
+
+@transaction.atomic
 def login_page(request):
     if request.user.is_authenticated:
         # TODO: Otra pagina o algo así
         return index(request)
     else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username,
+                                password=password,
+                                request=request)
+            if user is not None:
+                login(request, user)
+                return render(request, 'landing-page.html', user_context(request))
+            else:
+                return render(request, 'login.html')
         return render(request, 'UserSys/login.html')
-
 
 # Muestra la pagina de registro si el usuario no esta logeado
 @transaction.atomic
@@ -40,13 +68,22 @@ def register_page(request):
             usuario_form = UsuarioForm(request.POST)
             if user_form.is_valid() and usuario_form.is_valid():
                 email = user_form.data['email']
+                password = user_form.cleaned_data['password']
                 user = user_form.save(commit=False)
                 user.username = email
+                user.set_password(raw_password=password)
                 user.save()
                 user.refresh_from_db()
                 usuario_form = UsuarioForm(request.POST, instance=user.usuario)
                 usuario_form.full_clean()
                 usuario_form.save()
+                username = user.username
+                user = authenticate(username=username,
+                                    password=password,
+                                    request=request)
+                if user is not None:
+                    login(request, user)
+                    return render(request, 'landing-page.html', user_context(request))
                 return render(request, 'landing-page.html')
         else:
             user_form = UserForm()
@@ -56,30 +93,6 @@ def register_page(request):
                           'user_form': user_form,
                           'usuario_form': usuario_form
                       })
-
-
-# Logea al usuario dentro de la página y lo lleva al landing page que corresponda.
-def login_user(request):
-    user = ''
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = authenticate(request, username=email, password=password)
-    if user is not None:
-        if user.is_active:
-            login(request, user)
-        # TODO: Llevar al landing page admin/user segun el tipo de usuario.
-        return render(request, 'landing-page.html')
-    else:
-        # TODO: Crear la pagina de error (?)
-        return render(request, 'fail-page.html')
-
-
-# Desloguea al usuario y lo lleva al inicio.
-@login_required
-def logout_user(request):
-    logout(request)
-    return render(request, 'stater-page.html')
 
 
 def isNum(data):
@@ -100,15 +113,17 @@ def search_articulos(request):
     else:
         results = Articulo.objects.filter(nombre__icontains=search_term)
     context = {'articulos': results}
+    context = {**context, **user_context(request)} # Esto fusiona dos dict
     return render(request, 'resultados_articulos.html', context)
 
 
 def admin_landing(request):
-    context = {'usuarios' : Usuario.objects.all(),
+    context = {**{'usuarios' : Usuario.objects.all(),
                'articulos' : Articulo.objects.all(),
                'espacios' : Espacio.objects.all(),
                'pedidoespacios' : PedidoEspacio.objects.all(),
-               'pedidoarticulos' : PedidoArticulo.objects.all()}
+               'pedidoarticulos' : PedidoArticulo.objects.all(), },
+               **user_context(request)}
     return render(request, 'adminlanding.html', context)
 
 def cambiar_estado_pendientes(request):
@@ -122,11 +137,12 @@ def cambiar_estado_pendientes(request):
                 pedidoespacio = PedidoEspacio.objects.get(id_espacio=id)
                 pedidoespacio.estado = 2
                 pedidoespacio.save()
-        context = {'usuarios' : Usuario.objects.all(),
+        context = {**{'usuarios' : Usuario.objects.all(),
                    'articulos' : Articulo.objects.all(),
                    'espacios' : Espacio.objects.all(),
                    'pedidoespacios' : PedidoEspacio.objects.all().order_by('fecha_pedido'),
-                   'pedidoarticulos' : PedidoArticulo.objects.all().order_by('fecha_pedido')}
+                   'pedidoarticulos' : PedidoArticulo.objects.all().order_by('fecha_pedido')},
+                **user_context(request)}
         return render(request, 'adminlanding.html', context)
 
 def filtrar_prestamos(request):
@@ -141,11 +157,12 @@ def filtrar_prestamos(request):
         elif 'perdidos' in request.POST:
             pedidoarticulosfiltrados =PedidoArticulo.objects.filter(estado=PedidoArticulo.PERDIDO)
 
-        context = {'usuarios' : Usuario.objects.all(),
+        context = {**{'usuarios' : Usuario.objects.all(),
                    'articulos' : Articulo.objects.all(),
                    'espacios' : Espacio.objects.all(),
                    'pedidoespacios' : PedidoEspacio.objects.all().order_by('fecha_pedido'),
-                   'pedidoarticulos' : pedidoarticulosfiltrados.order_by('fecha_pedido')}
+                   'pedidoarticulos' : pedidoarticulosfiltrados.order_by('fecha_pedido')},
+            **user_context(request)}
         return render(request, 'adminlanding.html', context)
 
 def ficha_articulo(request):
