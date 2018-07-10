3e942734cdd215ded218bbc2a77b01f9a9778e9f
JoaquinPerezA <joaquin.perez.a@ug.uchile.cl>
2018-07-09 23:54:44 -0400

    Refactoring del codigo de la Navbar + Ahora la navbar lleva a donde dice que tiene que llevar

--- app/urls.py
+++ app/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     re_path(r'^searchArticulos/$', views.search_articulos, name='searchArticulos'),
     # re_path(r'^cambiar_estado_pendientes/$', views.cambiar_estado_pendientes, name='cambiar_estado_pendientes'),
     path('ficha_articulo', views.ficha_articulo, name='ficha_articulo'),
-    path('profile', views.user_profile, name='userProfile'),
+    path('profile', views.user_profile, name='user_profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
